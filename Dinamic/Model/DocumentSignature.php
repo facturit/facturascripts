017b61b42a46406d0e5a363281cc63a00f62572a
facturit <67974605+facturit@users.noreply.github.com>
2025-11-08 19:53:46 +0100

    Add biometric document signature capture

---
+++ Dinamic/Model/DocumentSignature.php
@@ -0,0 +1,8 @@
+<?php namespace FacturaScripts\Dinamic\Model;
+
+/**
+ * Class created to extend the core DocumentSignature model.
+ */
+class DocumentSignature extends \FacturaScripts\Core\Model\DocumentSignature
+{
+}
